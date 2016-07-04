0eb9d9dad90a79c71ee7f06c1f82884235503c71
ura soul <ura@localhost.localdomain>
2016-07-04 16:27:41 +0100

    updated for elgg 2.x + compatibility for hypeLists and tinymce

--- views/default/galliComments/js.php
+++ views/default/galliComments/js.php
@@ -2,34 +2,35 @@
 elgg.provide('elgg.galliComments');
 
 elgg.galliComments.init = function() {
-	var form = $('.elgg-form-comment-save');
-	form.find('input[type=submit]').live('click', elgg.galliComments.submit);	
+	$('.elgg-form-comment-save').find('input[type=submit]').on('click', elgg.galliComments.submit);
 };
 
 elgg.galliComments.submit = function(e) {
-	is_tinyMCE_active = false;
-	if ((typeof tinyMCE != "undefined") && tinyMCE.activeEditor && !tinyMCE.activeEditor.isHidden()) {
-		is_tinyMCE_active = true;
+	e.preventDefault();
+
+	is_tinymce_active = false;
+	if ((typeof tinymce != "undefined") && tinymce.activeEditor && !tinymce.activeEditor.isHidden()) {
+		is_tinymce_active = true;
 	}
-	if (is_tinyMCE_active) {
-		tinyMCE.triggerSave();
-	}	
+	if (is_tinymce_active) {
+		tinymce.triggerSave();
+	}
 	var form = $(this).parents('form');
 	var riverId = form.find('input[name=river_id]').val();
 	var data = form.serialize();
 	elgg.action('galliComments/add', {
 		data: data,
 		success: function(json) {
-			if(!riverId){		
-				var ul = $('.elgg-comments > ul.elgg-list');
-				// allow plugins to prepend comment when annotations are ordered by 'time_created desc' || Or perform own action 
-				// Thanks to Manutopik(https://github.com/ManUtopiK) for the hook 
-				var orderBy = elgg.trigger_hook('getOptions', 'galliComments.submit', json.output, 'asc');
+			if(!riverId){
+				var ul = $('.elgg-comments > ul.elgg-list, .elgg-comments > .elgg-list-container > ul.elgg-list');
+				// allow plugins to prepend comment when annotations are ordered by 'time_created desc' || Or perform own action
+				// Thanks to Manutopik(https://github.com/ManUtopiK) for the hook
+				var orderBy = elgg.trigger_hook('getOptions', 'elgg.galliComments.submit', json.output, 'asc');
 				if (orderBy ==  'asc') {
 					if (ul.length < 1) {
-						form.parent().prepend(json.output);
+						form.parent().append('<h3 id="comments">comments</h3><ul class="elgg-list elgg-list-entity"><li class="elgg-item elgg-item-object elgg-item-object-comment">' + json.output + '</li></ul>');
 					} else {
-						ul.append($(json.output));
+						ul.append($('<li class="elgg-item elgg-item-object elgg-item-object-comment">' + json.output + '</li>'));
 					}
 				} else if (orderBy == 'desc') {
 					if (ul.length < 1) {
@@ -38,9 +39,9 @@ elgg.galliComments.submit = function(e) {
 						ul.prepend($(json.output));
 					}
 				} // else if other than 'asc' or 'desc' lets plugin perform own action
-				if (is_tinyMCE_active) {
-					tinyMCE.activeEditor.setContent(''); 
-				} else {	
+				if (is_tinymce_active) {
+					tinymce.activeEditor.setContent('');
+				} else {
 					form.find('textarea').val('');
 				}
 			} else {
@@ -56,9 +57,8 @@ elgg.galliComments.submit = function(e) {
 						}
 					}
 				});
-			}	
+			}
 		}
 	});
-	e.preventDefault();
 };
 elgg.register_hook_handler('init', 'system', elgg.galliComments.init);
